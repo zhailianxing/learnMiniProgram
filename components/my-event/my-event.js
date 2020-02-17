2f20bd0b60939f833a8272a058c3b06ccb33c8a6
zhailianxing <575206613@qq.com>
2020-02-17 16:35:10 +0800

    组件传递事件给页面时，同时携带数据传递

--- components/my-event/my-event.js
+++ components/my-event/my-event.js
@@ -9,7 +9,7 @@ Component({
       //第一个参数：要传递给page的事件名字
       //第二个参数：是要传递的数据
       //第3个参数是 options,默认传递{}
-      this.triggerEvent("increment", {},{})
+      this.triggerEvent("increment", {age:18},{})
     }
   }
 })
--- pages/useComponents/useComponents.js
+++ pages/useComponents/useComponents.js
@@ -6,8 +6,9 @@ Page({
     count:0
   },
   
-  handleIncrement(){
+  handleIncrement(event){
     console.log("---------")
+    console.log(event.detail.age)
     this.setData({
       count: this.data.count+1
     })
